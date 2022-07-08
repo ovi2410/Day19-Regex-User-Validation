2f73c72540fc44ca56ac9dfdfbe7169f2e3f0b55
ovi2410 <guneshwarifegade@gmail.com>
2022-07-08 14:55:35 +0530

    [ovi]UC5_Validate_PassRule1

--- RegexValidation/Program.cs
+++ RegexValidation/Program.cs
@@ -7,13 +7,13 @@ Console.WriteLine("----- Regex User Validation -----\n");
 
 RegexOps pattern = new RegexOps();
 
-Console.Write("Enter Mobile Number of the User :- ");
-string User_MobNo = Console.ReadLine();
-if (pattern.ValidateUserName(User_MobNo))
+Console.Write("Enter Password of the User :- ");
+string User_Pass = Console.ReadLine();
+if (pattern.ValidateUserName(User_Pass))
 {
-    Console.WriteLine("\n>> User Mobile Number format is valid....");
+    Console.WriteLine("\n>> User Password format is valid....");
 }
 else
 {
-    Console.WriteLine("\n>> User Mobile Number format is Invalid....");
+    Console.WriteLine("\n>> User Password format is Invalid....");
 }
